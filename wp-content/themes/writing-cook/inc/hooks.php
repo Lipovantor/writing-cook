7a092f512c348a988e74b6f78510407a69325fc0
Lipovantor <lipovantor7@gmail.com>
2023-08-14 22:48:39 +0300

    Fix

--- wp-content/themes/writing-cook/inc/hooks.php
+++ wp-content/themes/writing-cook/inc/hooks.php
@@ -129,3 +129,146 @@ function recipe_search_ajax_handler() {
 }
 add_action('wp_ajax_recipe_search', 'recipe_search_ajax_handler');
 add_action('wp_ajax_nopriv_recipe_search', 'recipe_search_ajax_handler');
+
+
+
+
+// Фильтр
+// Функция для обработки AJAX-запроса на фильтрацию рецептов
+function filter_recipes() {
+  $selected_categories = explode(',', sanitize_text_field($_POST['categories']));
+  $page = max(1, intval($_POST['page'])); // Получаем номер страницы
+
+  $args = array(
+      'post_type' => 'recipes',
+      'post_status' => 'publish',
+      'posts_per_page' => 8,
+      'paged' => $page,
+      'tax_query' => array(
+          array(
+              'taxonomy' => 'recipe_category',
+              'field' => 'term_id',
+              'terms' => $selected_categories,
+              'operator' => 'IN',
+          ),
+      ),
+  );
+
+  $query = new WP_Query($args);
+
+  if ($query->have_posts()) {
+      while ($query->have_posts()) {
+          $query->the_post();
+          $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
+          $title = get_the_title();
+          $excerpt = get_the_excerpt();
+          ?>
+          <a class="recipe-card" href="<?php echo get_permalink() ?>">
+            
+            <?php if ($thumbnail_url) { ?>
+              <div class="recipe-card__image">
+                <img src="<?php echo $thumbnail_url ?>" 
+                  alt="" 
+                  width="360"
+                  height="250"/>
+              </div>
+            <?php } else { ?>
+              <div class="recipe-card__plug">
+              <?php 
+                $image = get_field('card_plug', 'option');
+                if( !empty( $image ) ) { ?>
+                    <img src="<?php echo esc_url($image['url']); ?>" 
+                          alt="<?php echo esc_attr($image['alt']); ?>" 
+                          width="360"
+                          height="250"/>
+                <?php } ?>
+              </div>
+            <?php } ?>
+            <div class="recipe-card__box">
+              <div class="recipe-card__content recipe-card__content_main">
+                <div class="recipe-card__header">
+                  <div class="recipe-card__inner recipe-card__inner_title">
+                    <h3 class="recipe-card__title">
+                      <?php echo $title ?>
+                    </h3>
+                  </div>
+                  <div class="recipe-card__inner recipe-card__inner_excerpt">
+                    <p class="recipe-card__excerpt">
+                      <?php echo $excerpt ?>
+                    </p>
+                  </div>
+                </div>
+                <script>
+                  function truncateText() {
+                    const card = $('.recipe-card');
+                    const text = $('.recipe-card__title');
+                    const cardHeight = card.height();
+
+                    while (text[0].scrollHeight > cardHeight) {
+                      text.text(function (index, text) {
+                        return text.replace(/\W*\s(\S)*$/, '...');
+                      });
+                    }
+                  }
+
+                  $(window).on('load resize', truncateText);
+                </script>
+              </div>
+              
+              <button class="recipe-card__button" aria-label="Развернуть-свернуть карточку рецепта">
+                <img src="<?php echo WRC_THEME_URI . '/dist/img/icons/arrow-down.svg'; ?>" alt="" width="13" height="9">
+              </button>
+              <div class="recipe-card__content recipe-card__content_second">
+                <div class="recipe-card__ingredients">
+                  <p class="recipe-card__ingredients-title">Ингредиенты и инвентарь</p>
+                  <p class="recipe-card__ingredients-title">на 4 порции</p>
+                  <ul class="recipe-card__ingredients-list">
+                    <li class="recipe-card__ingredients-item">Горох</li>
+                    <li class="recipe-card__ingredients-item">Вода</li>
+                    <li class="recipe-card__ingredients-item">Сухари</li>
+                    <li class="recipe-card__ingredients-item">Петрушка</li>
+                    <li class="recipe-card__ingredients-item">Морковь</li>
+                    <li class="recipe-card__ingredients-item">Горох</li>
+                    <li class="recipe-card__ingredients-item">Вода</li>
+                    <li class="recipe-card__ingredients-item">Сухари</li>
+                    <li class="recipe-card__ingredients-item">Петрушка</li>
+                    <li class="recipe-card__ingredients-item">Морковь</li>
+                  </ul>
+                </div>
+                <div class="recipe-card__meta">
+                  <div class="recipe-card__author">
+                    <?php echo get_the_author(); ?>
+                  </div>
+                  <div class="recipe-card__bookmark">
+                    3
+                  </div>
+                  <div class="recipe-card__like">
+                    +
+                  </div>
+                </div>
+              </div>
+
+              
+            </div>
+          </a>
+        <?php
+      }
+
+      echo '<div class="searching-results__pagination">';
+      echo paginate_links(array(
+          'current' => $page,
+          'total' => $query->max_num_pages,
+          'prev_next' => false,
+      ));
+      echo '</div>';
+
+      wp_reset_postdata();
+  } else {
+      echo '<div class="container">Рецепты не найдены</div>';
+  }
+
+  die();
+}
+
+add_action('wp_ajax_filter_recipes', 'filter_recipes'); // Для зарегистрированных пользователей
+add_action('wp_ajax_nopriv_filter_recipes', 'filter_recipes'); // Для незарегистрированных пользователей
--- wp-content/themes/writing-cook/template-parts/global-sections/intro.php
+++ wp-content/themes/writing-cook/template-parts/global-sections/intro.php
@@ -24,6 +24,7 @@ if (!defined('ABSPATH')) {
 
 <section class="searching-results"></section>
 
+<!-- JS Search -->
 <script>
 jQuery(document).ready(function($) {
   $('.search-form-recipes').submit(function(event) {
@@ -42,137 +43,199 @@ jQuery(document).ready(function($) {
   });
 });
 </script>
+<!-- JS Search END -->
+
+<!-- Filter recipes -->
+<div class="category-filter">
+    <?php
+    $categories = get_terms(array(
+        'taxonomy' => 'recipe_category',
+        'orderby' => 'name',
+        'order' => 'ASC',
+    ));
+
+    foreach ($categories as $category) {
+        echo '<label><input type="checkbox" class="category-checkbox" value="' . esc_attr($category->term_id) . '"> ' . esc_html($category->name) . '</label>';
+    }
+    ?>
+    <input type="hidden" id="category-filter-values" name="category_filter_values" value="">
+</div>
+
+<script>
+jQuery(document).ready(function($) {
+    let selectedCategories = []; // Инициализация массива для выбранных категорий
+    
+    // Обработчик изменения чекбоксов
+    $('.category-checkbox').on('change', function() {
+        selectedCategories = $('.category-checkbox:checked')
+            .map(function() {
+                return $(this).val();
+            })
+            .get();
+        updateRecipes(selectedCategories);
+    });
+
+    // Обработчик клика по пагинации
+    $(document).on('click', '.searching-results__pagination a', function(e) {
+    e.preventDefault(); // Предотвращаем стандартное поведение ссылок
+    if ($(this).hasClass('current')) return; // Если кнопка уже активная, ничего не делаем
+    const page = parseInt($(this).text()); // Получаем номер страницы из текста кнопки
+    updateRecipes(selectedCategories, page);
+});
+
+    
+    // Функция обновления рецептов через AJAX
+    function updateRecipes(selectedCategories, page = 1) {
+    $.ajax({
+        url: '<?php echo esc_url(admin_url('admin-ajax.php')); ?>',
+        type: 'POST',
+        data: {
+            action: 'filter_recipes',
+            categories: selectedCategories.join(','), // Преобразование массива в строку
+            page: page
+        },
+        success: function(response) {
+            $('.searching-results__list').html(response);
+            $(window).scrollTop(0); // Прокручиваем страницу вверх после обновления
+        }
+    });
+}
+});
+
+</script>
+<!-- Filter recipes END -->
 
 <?php
-$paged = ( get_query_var('page') ) ? get_query_var('page') : 1; // Получаем номер текущей страницы.
+// $paged = ( get_query_var('page') ) ? get_query_var('page') : 1;
+$paged = max( 1, intval(get_query_var('paged')) );
+
 
 $args = array(
     'post_type' => 'recipes',
     'post_status' => 'publish',
-    'posts_per_page' => 8, // Ограничиваем количество постов на странице до 4.
+    'posts_per_page' => 8,
     'paged' => $paged,
 );
-  
-  $query = new WP_Query($args);
-  
-  if ($query->have_posts()) { ?> 
 
-    <section>
-      <div class="container">
-        <div class="searching-results__list">
-          <?php
-          while ($query->have_posts()) {
-            $query->the_post();
-            $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
-            $title = get_the_title();
-            $excerpt = get_the_excerpt();
-            ?>
-            <a class="recipe-card" href="<?php echo get_permalink() ?>">
-              
-              <?php if ($thumbnail_url) { ?>
-                <div class="recipe-card__image">
-                  <img src="<?php echo $thumbnail_url ?>" 
-                    alt="" 
-                    width="360"
-                    height="250"/>
-                </div>
-              <?php } else { ?>
-                <div class="recipe-card__plug">
-                <?php 
-                  $image = get_field('card_plug', 'option');
-                  if( !empty( $image ) ) { ?>
-                      <img src="<?php echo esc_url($image['url']); ?>" 
-                            alt="<?php echo esc_attr($image['alt']); ?>" 
-                            width="360"
-                            height="250"/>
-                  <?php } ?>
-                </div>
-              <?php } ?>
-              <div class="recipe-card__box">
-                <div class="recipe-card__content recipe-card__content_main">
-                  <div class="recipe-card__header">
-                    <div class="recipe-card__inner recipe-card__inner_title">
-                      <h3 class="recipe-card__title">
-                        <?php echo $title ?>
-                      </h3>
-                    </div>
-                    <div class="recipe-card__inner recipe-card__inner_excerpt">
-                      <p class="recipe-card__excerpt">
-                        <?php echo $excerpt ?>
-                      </p>
-                    </div>
-                  </div>
-                  <script>
-                    function truncateText() {
-                      const card = $('.recipe-card');
-                      const text = $('.recipe-card__title');
-                      const cardHeight = card.height();
+$query = new WP_Query($args);
 
-                      while (text[0].scrollHeight > cardHeight) {
-                        text.text(function (index, text) {
-                          return text.replace(/\W*\s(\S)*$/, '...');
-                        });
-                      }
-                    }
+if ($query->have_posts()) { ?> 
 
-                    $(window).on('load resize', truncateText);
-                  </script>
-                </div>
-                
-                <button class="recipe-card__button" aria-label="Развернуть-свернуть карточку рецепта">
-                  <img src="<?php echo WRC_THEME_URI . '/dist/img/icons/arrow-down.svg'; ?>" alt="" width="13" height="9">
-                </button>
-                <div class="recipe-card__content recipe-card__content_second">
-                  <div class="recipe-card__ingredients">
-                    <p class="recipe-card__ingredients-title">Ингредиенты и инвентарь</p>
-                    <p class="recipe-card__ingredients-title">на 4 порции</p>
-                    <ul class="recipe-card__ingredients-list">
-                      <li class="recipe-card__ingredients-item">Горох</li>
-                      <li class="recipe-card__ingredients-item">Вода</li>
-                      <li class="recipe-card__ingredients-item">Сухари</li>
-                      <li class="recipe-card__ingredients-item">Петрушка</li>
-                      <li class="recipe-card__ingredients-item">Морковь</li>
-                      <li class="recipe-card__ingredients-item">Горох</li>
-                      <li class="recipe-card__ingredients-item">Вода</li>
-                      <li class="recipe-card__ingredients-item">Сухари</li>
-                      <li class="recipe-card__ingredients-item">Петрушка</li>
-                      <li class="recipe-card__ingredients-item">Морковь</li>
-                    </ul>
-                  </div>
-                  <div class="recipe-card__meta">
-                    <div class="recipe-card__author">
-                      <?php echo get_the_author(); ?>
-                    </div>
-                    <div class="recipe-card__bookmark">
-                      3
-                    </div>
-                    <div class="recipe-card__like">
-                      +
-                    </div>
-                  </div>
-                </div>
-
-                
+  <section>
+    <div class="container">
+      <div class="searching-results__list">
+        <?php
+        while ($query->have_posts()) {
+          $query->the_post();
+          $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
+          $title = get_the_title();
+          $excerpt = get_the_excerpt();
+          ?>
+          <a class="recipe-card" href="<?php echo get_permalink() ?>">
+            
+            <?php if ($thumbnail_url) { ?>
+              <div class="recipe-card__image">
+                <img src="<?php echo $thumbnail_url ?>" 
+                  alt="" 
+                  width="360"
+                  height="250"/>
               </div>
-            </a>
-            <?php
-          }
+            <?php } else { ?>
+              <div class="recipe-card__plug">
+              <?php 
+                $image = get_field('card_plug', 'option');
+                if( !empty( $image ) ) { ?>
+                    <img src="<?php echo esc_url($image['url']); ?>" 
+                          alt="<?php echo esc_attr($image['alt']); ?>" 
+                          width="360"
+                          height="250"/>
+                <?php } ?>
+              </div>
+            <?php } ?>
+            <div class="recipe-card__box">
+              <div class="recipe-card__content recipe-card__content_main">
+                <div class="recipe-card__header">
+                  <div class="recipe-card__inner recipe-card__inner_title">
+                    <h3 class="recipe-card__title">
+                      <?php echo $title ?>
+                    </h3>
+                  </div>
+                  <div class="recipe-card__inner recipe-card__inner_excerpt">
+                    <p class="recipe-card__excerpt">
+                      <?php echo $excerpt ?>
+                    </p>
+                  </div>
+                </div>
+                <script>
+                  function truncateText() {
+                    const card = $('.recipe-card');
+                    const text = $('.recipe-card__title');
+                    const cardHeight = card.height();
 
-          // Add Pagination
-          echo paginate_links( [
-            // 'base'    => user_trailingslashit( wp_normalize_path( get_permalink() .'/%#%/' ) ),
+                    while (text[0].scrollHeight > cardHeight) {
+                      text.text(function (index, text) {
+                        return text.replace(/\W*\s(\S)*$/, '...');
+                      });
+                    }
+                  }
+
+                  $(window).on('load resize', truncateText);
+                </script>
+              </div>
+              
+              <button class="recipe-card__button" aria-label="Развернуть-свернуть карточку рецепта">
+                <img src="<?php echo WRC_THEME_URI . '/dist/img/icons/arrow-down.svg'; ?>" alt="" width="13" height="9">
+              </button>
+              <div class="recipe-card__content recipe-card__content_second">
+                <div class="recipe-card__ingredients">
+                  <p class="recipe-card__ingredients-title">Ингредиенты и инвентарь</p>
+                  <p class="recipe-card__ingredients-title">на 4 порции</p>
+                  <ul class="recipe-card__ingredients-list">
+                    <li class="recipe-card__ingredients-item">Горох</li>
+                    <li class="recipe-card__ingredients-item">Вода</li>
+                    <li class="recipe-card__ingredients-item">Сухари</li>
+                    <li class="recipe-card__ingredients-item">Петрушка</li>
+                    <li class="recipe-card__ingredients-item">Морковь</li>
+                    <li class="recipe-card__ingredients-item">Горох</li>
+                    <li class="recipe-card__ingredients-item">Вода</li>
+                    <li class="recipe-card__ingredients-item">Сухари</li>
+                    <li class="recipe-card__ingredients-item">Петрушка</li>
+                    <li class="recipe-card__ingredients-item">Морковь</li>
+                  </ul>
+                </div>
+                <div class="recipe-card__meta">
+                  <div class="recipe-card__author">
+                    <?php echo get_the_author(); ?>
+                  </div>
+                  <div class="recipe-card__bookmark">
+                    3
+                  </div>
+                  <div class="recipe-card__like">
+                    +
+                  </div>
+                </div>
+              </div>
+
+              
+            </div>
+          </a>
+        <?php } ?>
+        <!-- Pagination -->
+        <div class="searching-results__pagination">
+          <?php echo paginate_links( [
             'current' => max( 1, $paged ),
             'total'   => $query->max_num_pages,
-          ] );
-
-          wp_reset_postdata();
-          ?>
+            'prev_next' => false,
+          ] ); ?>
         </div>
+        <!-- Pagination END -->
+        <?php wp_reset_postdata(); ?>
       </div>
-    </section>
-  <?php } else {
-    echo '<div class="container">Рецепты не найдены</div>';
-  } 
+    </div>
+  </section>
+<?php } else {
+  echo '<div class="container">Рецепты не найдены</div>';
+} 
 ?>
 
 
@@ -187,34 +250,3 @@ $args = array(
     card.toggleClass('recipe-card_active')
   });
 </script>
-
-<div>
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex amet voluptatum commodi neque! Doloremque, nulla eveniet qui cupiditate magni porro, ut eos ipsa deserunt commodi cum, a amet iure laborum.
-</div>
